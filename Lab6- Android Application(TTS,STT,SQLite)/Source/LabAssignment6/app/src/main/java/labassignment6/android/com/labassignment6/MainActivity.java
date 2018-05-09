c2ad8abca84ae9dec49e659e751f002ea540e354
SadanandKallakuri <sadanand2u@gmail.com>
2018-05-08 19:49:18 -0500

    commit

---
+++ Lab6- Android Application(TTS,STT,SQLite)/Source/LabAssignment6/app/src/main/java/labassignment6/android/com/labassignment6/MainActivity.java
@@ -0,0 +1,13 @@
+package labassignment6.android.com.labassignment6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
